416991175f6cc3b705e47e04543841365dc08c63
Logan Su <logansu1025@outlook.com>
2026-03-07 17:52:32 -0600

    Connect to Inngest

--- main.py
+++ main.py
@@ -12,11 +12,19 @@ load_dotenv()
 
 inngest_client = inngest.Inngest(
     app_id="RAG_Agent",
-    logging=logging.getLogger("uvicorn"),
+    logger=logging.getLogger("uvicorn"),
     is_production=False,
     serializer=inngest.PydanticSerializer()
 )
 
+
+@inngest_client.create_function(
+    fn_id="RAG: Ingest PDF",
+    trigger=inngest.TriggerEvent(event="rag/ingest_pdf")
+)
+async def rag_ingest_pdf(ctx: inngest.Context):
+    return {"hello": "world"}
+
 app = FastAPI()
 
-inngest.fast_api.serve(app, inngest_client, [])
+inngest.fast_api.serve(app, inngest_client, [rag_ingest_pdf])
